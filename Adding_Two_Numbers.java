2f3d0f33c3991222b8d779683b4c1ef595c2ea17
DeviKaraka <155526308+DeviKaraka@users.noreply.github.com>
2024-07-31 15:36:25 +0530

    import java.util.Scanner; public class Division{     public static void main(String[] args)     {         int a,b,c;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         c=a+b;         System.out.println(c);     } }

--- Adding_Two_Numbers.java
+++ Adding_Two_Numbers.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-public class Sum{
+public class Division{
     public static void main(String[] args)
     {
         int a,b,c;
         Scanner read=new Scanner(System.in);
         a=read.nextInt();
         b=read.nextInt();
-
         c=a+b;
         System.out.println(c);
     }
